51c9688b05199f9a98cbf23ee39ef55e7ab0a234
Xenko10 <kubinios_2002@o2.pl>
2024-03-12 11:04:18 +0100

    create dedicated folder for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Hero from "./components/Hero/Hero";
-import FlashSales from "./components/FlashSales/FlashSales";
+import Home from "./home/page";
 
 export default function App() {
   return (
     <>
-      <Hero />
-      <FlashSales />
+      <Home />
     </>
   );
 }
