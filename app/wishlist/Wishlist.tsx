3226cd4eb9931d9a5b22d1f54a7b29529d16a57e
Xenko10 <kubinios_2002@o2.pl>
2024-03-15 11:15:50 +0100

    display how many items are in wishlist

--- app/wishlist/Wishlist.tsx
+++ app/wishlist/Wishlist.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import styles from "./Wishlist.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "../../constant";
 
 export default function Wishlist() {
+  const [wishlist, setWishlist] = useState<{ id: number }[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get(`${API_URL}/wishlist`).then((response) => {
+        const data = response.data;
+        setWishlist(data);
+      });
+    };
+    fetchData();
+  }, []);
   return (
     <div className={styles.wishlist}>
-      <h1>Wishlist</h1>
+      <h2 className={styles.header}>
+        Wishlist {wishlist.length !== 0 ? `(${wishlist.length})` : null}
+      </h2>
     </div>
   );
 }
