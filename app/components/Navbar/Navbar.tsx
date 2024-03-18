838a62c205705ede86ee561d8bff8a6b039d828d
Xenko10 <kubinios_2002@o2.pl>
2024-03-18 09:35:17 +0100

    make Navbar bottom border take 100vw

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,27 +4,29 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <Link href='/'>Exclusive</Link>
-      </div>
-      <div className={styles.menu}>
-        <Link href='/'>Home</Link>
-        <a href='/contact'>Contact</a>
-        <a href='/about'>About</a>
-        <a href='/signup'>Sign Up</a>
-      </div>
-      <button>☰</button>
-      <div className={styles.actions}>
-        <div className={styles.search}>
-          <input type='text' placeholder='What are you looking for?' />
-          <img src='./img/navbar/search.png' alt='search' />
+      <div className={styles.logoActionsWrapper}>
+        <div className={styles.logo}>
+          <Link href='/'>Exclusive</Link>
         </div>
-        <Link href='/wishlist'>
-          <img src='./img/navbar/heart.png' alt='heart' />
-        </Link>
-        <Link href='/cart'>
-          <img src='./img/navbar/cart.png' alt='cart' />
-        </Link>
+        <div className={styles.menu}>
+          <Link href='/'>Home</Link>
+          <a href='/contact'>Contact</a>
+          <a href='/about'>About</a>
+          <a href='/signup'>Sign Up</a>
+        </div>
+        <button>☰</button>
+        <div className={styles.actions}>
+          <div className={styles.search}>
+            <input type='text' placeholder='What are you looking for?' />
+            <img src='./img/navbar/search.png' alt='search' />
+          </div>
+          <Link href='/wishlist'>
+            <img src='./img/navbar/heart.png' alt='heart' />
+          </Link>
+          <Link href='/cart'>
+            <img src='./img/navbar/cart.png' alt='cart' />
+          </Link>
+        </div>
       </div>
     </div>
   );
